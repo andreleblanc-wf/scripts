abcfdfcdbbf63b0b6bf87c50ee6a0db0545a7007
Andre LeBlanc <andre.leblanc@webfilings.com>
2014-04-23 11:40:00 -0400

    added formatter and grep options

--- runserver.py
+++ runserver.py
@@ -1,8 +1,20 @@
+#!/usr/bin/env python
+import datetime
+import re
+
 import subprocess
 import sys
 import signal
 import os
 import settingslocal
+import argparse
+
+parser = argparse.ArgumentParser("Run the bigsky dev server")
+parser.add_argument('-g', '--grep', metavar='REGEX', type=str, nargs=1, dest='search_re')
+parser.add_argument('runserver_args', metavar='RUNSERVER', type=str, nargs='*')
+parser.add_argument('-f', '--formatter', metavar='FORMATTER', type=str, nargs=1, dest='formatter', default='format_log_advanced')
+
+log_re = re.compile(r'^(WARNING|DEBUG|INFO|ERROR)\s+(\d\d\d\d)\-(\d\d)\-(\d\d)\s(\d?\d):(\d\d):(\d\d),(\d\d\d)\s([\w\d_\.]+):(\d+)\]\s(.*)$')
 
 manage_py_process = "python manage.py runserver"
 
@@ -17,18 +29,46 @@ class color:
     RED = '\033[91m'
     ENDC = '\033[0m'
 
-class custom_color:
-    WARNING = color.YELLOW
-    DEBUG = color.BLUE
-    INFO = color.GREEN
-    ERROR = color.RED
+COLORS = dict(
+    WARNING=color.YELLOW,
+    DEBUG=color.BLUE,
+    INFO=color.GREEN,
+    ERROR=color.RED
+)
 
-def main():
-    process = subprocess.Popen("%s %s" % (manage_py_process, (' '.join(sys.argv[1:]))), shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+def format_log_advanced(log_level, log_datetime, filename, file_line, message):
+
+    return "{level_color}{level}\t{datetime:%H}:{datetime:%M}:{datetime:%S} [{filename}:{file_line}]{endc} {message}".format(
+        level=log_level,
+        datetime=log_datetime,
+        filename=filename,
+        file_line=file_line,
+        message=message,
+        level_color=COLORS[log_level],
+        endc=color.ENDC
+    )
+
+def main(args):
+
+    if args.search_re is not None:
+        matches = []
+        search_re = re.compile(re.escape(args.search_re[0]))
+
+    if args.formatter is None:
+        formatter = format_log_advanced
+    else:
+        formatter = globals()[args.formatter]
+
+    process = subprocess.Popen("%s %s" % (manage_py_process, (' '.join(args.runserver_args))), shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     output = process.stderr
 
     def signal_handler(input_signal, frame):
         os.kill(process.pid, signal.SIGTERM)
+        if matches:
+            print "\nCAPTURED OUTPUT"
+            print "---------------"
+            for m in matches:
+                print m
         sys.exit(0)
 
     signal.signal(signal.SIGINT, signal_handler)
@@ -38,25 +78,28 @@ def main():
         line = output.readline().strip('\n')
         if line:
             blank_lines = 0
-            split_location = str.find(line, ']') + 1
-            first_part = line[:split_location]
-            second_part = line[split_location:]
-    	
-            if line.startswith('WARNING'):
-                print custom_color.WARNING + first_part + color.ENDC + second_part 
-            elif line.startswith('DEBUG'):
-                print custom_color.DEBUG + first_part + color.ENDC + second_part 
-            elif line.startswith('INFO'):
-                print custom_color.INFO + first_part + color.ENDC + second_part
-            elif line.startswith('ERROR'):
-                print custom_color.ERROR + first_part + color.ENDC + second_part 
-            else:
-                print line
-        else:
-            print line
-            blank_lines += 1
-            if blank_lines > 4:
-                sys.exit(0)
+            m = log_re.match(line)
+            if m:
+                level, year, month, day, hour, minute, sec, msec, filename, file_line, msg = m.groups()
+
+                if search_re and search_re.search(msg):
+                    msg = color.PINK + msg + color.ENDC
+                    matches.append(formatter(level, log_timestamp, filename, file_line, msg))
+
+                log_timestamp = datetime.datetime(int(year), int(month), int(day),
+                      int(hour), int(minute), int(sec), int(msec) * 1000)
+                print formatter(level, log_timestamp, filename, file_line, msg)
+                continue
+
+        print '\t' + line
+        blank_lines += 1
+        if blank_lines > 4:
+            try:
+                process.kill()
+            except Exception:
+                pass
+            sys.exit(0)
 
 if __name__ == '__main__':
-    main()
+    args = parser.parse_args()
+    main(args)
